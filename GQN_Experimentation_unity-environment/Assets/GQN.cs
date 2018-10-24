5cb4d55d8a213f8a4f5c6f898815f1508f29cb20
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-10-24 10:52:03 +0200

    new after merge

---
+++ GQN_Experimentation_unity-environment/Assets/GQN.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TensorFlow;
+
+public class GQN : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        using (var session = new TFSession())
+        {
+            var graph = session.Graph;
+
+            var a = graph.Const(4);
+            var b = graph.Const(54);
+            
+            var imageInput = graph.Placeholder(TFDataType.Int32);
+
+            var result = session.Run()
+
+            //var addingResult = session.GetRunner().Run(graph.Add(a, b));
+            //var addingResultValue = addingResult.GetValue();
+
+            print(result.GetValue());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
