a9498192776e38c2c0793a997f93279f552dbc93
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-11-06 19:28:36 +0100

    fix parse training data

--- SimpleGQN/SimpleGQN.py
+++ SimpleGQN/SimpleGQN.py
@@ -54,16 +54,27 @@ def get_coordinates_from_filename(string):
         rotations.append(float(num.replace(',', '.')))
     return coordinates, rotations
 
-def get_data(data_dirs, num_data_points=None):
+
+def delete_corrupt_files(corrupt_files, ask=True):
+    if len(corrupt_files) > 0:
+        if input(f'{len(corrupt_files)} out of {len(data_paths)} files could not be opened. '
+                 f'The files might be corrup. Delete them? y/n\n') == 'y':
+            for file in corrupt_files:
+                os.remove(file)
+            print(f'{len(corrupt_files)} files deleted.')
+
+
+EnvData = collections.namedtuple('EnvData', 'images coordinates rotations')
+def get_data_for_environments(data_dirs, num_envs_to_load=None, num_data_from_env=None, randomized_draw=True):
     data_dirs = [data_dirs + '\\' + x for x in os.listdir(data_dirs)]
+    if randomized_draw:
+        random.shuffle(data_dirs)
     env_data = []
-    for data_paths in data_dirs:
+    for data_paths in data_dirs[0:num_envs_to_load]:
         data_paths = glob.glob(data_paths + '/*')
-        print(f'loading {num_data_points if num_data_points else len(data_paths)} data points.')
-        images = []
-        coordinates = []
-        rotations = []
-        corrupt_files = []
+        if randomized_draw:
+            random.shuffle(data_paths)
+        images, coordinates, rotations, corrupt_files = [], [], [], []
         for i, dp in enumerate(data_paths):
             coordinate, rotation = get_coordinates_from_filename(dp)
             try:
@@ -73,22 +84,17 @@ def get_data(data_dirs, num_data_points=None):
                 continue
             coordinates.append(coordinate)
             rotations.append(rotation)
-            if num_data_points and i+1 >= num_data_points:
+            if num_data_from_env and i+1 >= num_data_from_env:
                 break
-        env_data.append((images, coordinates, rotations))
-    if len(corrupt_files) > 0:
-        if input(f'{len(corrupt_files)} out of {len(data_paths)} files could not be opened. '
-                 f'The files might be corrup. Delete them? y/n\n') == 'y':
-            for file in corrupt_files:
-                os.remove(file)
-            print(f'{len(corrupt_files)} files deleted.')
+        env_data.append(EnvData(images, coordinates, rotations))
+        print(f'loaded {len(images)} entrys from  environment '
+              f'{len(env_data)}/{num_envs_to_load if num_envs_to_load else len(data_dirs)}', end='\n')
+    delete_corrupt_files(corrupt_files)
     return env_data
 
 
-def normalize_data(images, coordinates, rotations):
-    return np.array(images) / np.max(images), \
-           np.array(coordinates) / np.max(coordinates), \
-           np.array(rotations) / np.max(rotations)
+def normalize_data(data_tuple):
+    return (np.asarray(x) / np.max(x) for x in data_tuple)
 
 
 def product(iterable):
@@ -113,13 +119,13 @@ def autoencoder(picture_input_shape):
 
 
 def get_gqn_model(picture_input_shape, coordinates_input_shape):
+    print('creating model')
     # TODO Add ability to add up multiple observations to the latent representation
     number_of_pixels = product(picture_input_shape)
 
     picture_input = keras.Input(picture_input_shape, name='picture_input')
     coordinates_input = keras.Input(coordinates_input_shape, name='coordinates_input')
 
-    #x = keras.layers.Dense(1000, 'relu', True)(picture_input)
     x = keras.layers.Dense(1024, 'relu', True)(picture_input)
     for _ in range(9):
         x = keras.layers.Dense(1024, 'relu', True)(x)
@@ -132,18 +138,24 @@ def get_gqn_model(picture_input_shape, coordinates_input_shape):
 
     model = keras.Model(inputs=[picture_input, coordinates_input], outputs=predictions)
     model.compile('rmsprop', 'mse')
+
+    print('model created')
     return model
 
 
-def network_inputs_from_coordinates(position_datas, rotation_datas):
+# TODO test: might not work anymore
+def network_inputs_from_coordinates_vect(position_datas, rotation_datas):
     coordinates = []
     for p, r in zip(position_datas, rotation_datas):
-        coordinates_vec = []
-        coordinates_vec.extend(list(p))
-        coordinates_vec.extend(list(r))
-        coordinates_vec = np.reshape(coordinates_vec, -1)
+        network_inputs_from_coordinates_single(p, r)
         coordinates.append(coordinates_vec)
-    return np.array(coordinates)
+    return np.asarray(coordinates)
+
+def network_inputs_from_coordinates_single(position_data, rotation_data):
+    coordinates_vec = []
+    coordinates_vec.extend(list(position_data))
+    coordinates_vec.extend(list(rotation_data))
+    return np.asarray(coordinates_vec)
 
 
 def get_unique_model_save_name(img_shape, name=''):
@@ -173,7 +185,7 @@ def draw_autoencoding_evaluation():
 
 class CharacterController:
     def __init__(self, position_data_un):
-        self.center_pos = np.array([0, 1.5, 0]) / np.argmax(position_data_un)
+        self.center_pos = np.asarray([0, 1.5, 0]) / np.argmax(position_data_un)
         self.current_position = self.center_pos
         self.current_y_rotation = 0
         self.prev_time = 0
@@ -186,7 +198,7 @@ class CharacterController:
             rot -= np.pi * (int(rot / np.pi))
         if rot < 0:
             rot += np.pi * (1 + int(rot / np.pi))
-        return np.array([0, np.sin(rot), 0, np.cos(rot)])
+        return np.asarray([0, np.sin(rot), 0, np.cos(rot)])
 
     @property
     def current_rotation_quaternion(self):
@@ -197,9 +209,9 @@ class CharacterController:
         self.prev_time = time.time()
 
         current_rot_x2 = self.y_rot_to_quaternion(self.current_y_rotation * 2)
-        forward_vec = np.array([np.sin(current_rot_x2[1]), 0., np.sin(current_rot_x2[3])])
+        forward_vec = np.asarray([np.sin(current_rot_x2[1]), 0., np.sin(current_rot_x2[3])])
         current_rot_x2_90_deg_offset = self.y_rot_to_quaternion(self.current_y_rotation * 2 + np.pi / 2)
-        right_vec = -1 * np.array([np.sin(current_rot_x2_90_deg_offset[1]), 0., np.sin(current_rot_x2_90_deg_offset[3])])
+        right_vec = -1 * np.asarray([np.sin(current_rot_x2_90_deg_offset[1]), 0., np.sin(current_rot_x2_90_deg_offset[3])])
 
         pygame.event.pump()
         keys = pygame.key.get_pressed()
@@ -222,30 +234,38 @@ class CharacterController:
             self.current_position = self.center_pos
 
 
-def train_model(flat_image_inputs, coordinate_inputs, model_save_file_path, model_to_train=None, batch_size=None):
+def train_model(network_inputs, model_save_file_path, model_to_train, environment_batch_size=None, batch_size=None):
     model_save_file_path = remove_extension(model_save_file_path)
     checkpoint_save_path = f'{model_save_file_path}.checkpoint'
     model_final_save = f'{model_save_file_path}.hdf5'
 
-    if not model_to_train:
-        model_to_train = get_gqn_model(np.shape(flat_image_inputs[0]), np.shape(coordinate_inputs[0]))
     epochs = 500
     sub_epochs = 2
     training_aborted = False
     for i in range(int(epochs / sub_epochs)):
-        start_time = time.time()
-        scrampled_flat_image_inputs = np.random.permutation(flat_image_inputs)
-        model_to_train.fit([scrampled_flat_image_inputs, coordinate_inputs], flat_image_inputs, batch_size=batch_size,
-                      epochs=sub_epochs, callbacks=[
-                keras.callbacks.EarlyStopping(monitor='loss', patience=4),
-                keras.callbacks.LambdaCallback(on_epoch_end=lambda _, _b: print(f'\n\nTrueEpoch {i*sub_epochs}/{epochs}')),
-                keras.callbacks.ModelCheckpoint(checkpoint_save_path, period=sub_epochs)
-            ])
+        random.shuffle(network_inputs)
+        for i, (flat_image_inputs, coordinate_inputs) in enumerate(network_inputs):
+            if environment_batch_size and i > environment_batch_size:
+                break
+            flat_image_inputs = np.asarray(flat_image_inputs)
+            coordinate_inputs = np.asarray(coordinate_inputs)
+            # idxs = np.random.permutation((np.indices(flat_image_inputs)[0],))
+            # scrambled_flat_image_inputs = np.select(flat_image_inputs, idxs)
+            # scrambled_coordinate_inputs = np.select(coordinate_inputs, idxs)
+            scrambled_flat_image_inputs_2 = np.random.permutation(flat_image_inputs)
+
+            model_to_train.fit([scrambled_flat_image_inputs_2, coordinate_inputs], flat_image_inputs, batch_size=batch_size,
+                                epochs=sub_epochs
+                #                 callbacks=[
+                #                     keras.callbacks.EarlyStopping(monitor='loss', patience=4),
+                #                     keras.callbacks.LambdaCallback(on_epoch_end=lambda _, _b: print(f'\n\nTrueEpoch {i*sub_epochs}/{epochs}')),
+                #                     keras.callbacks.ModelCheckpoint(checkpoint_save_path, period=sub_epochs)
+                # ]
+            )
         if keyboard.is_pressed('q'):
             print('learning aborted by user')
             training_aborted = True
             break
-
     print('saving model')
     model_to_train.save(model_final_save)
     if not training_aborted:
@@ -256,7 +276,8 @@ def train_model(flat_image_inputs, coordinate_inputs, model_save_file_path, mode
 
 # TODO clean up and split up run method
 def run(data_dirs, model_save_file_path, image_dim, load_model_path=None, train=True,
-        num_samples_to_load=None, batch_size=None, run_environment=True, black_n_white=True):
+        num_envs_to_load=None, num_data_from_env=None, batch_size=None, run_environment=True, black_n_white=True, window_size=(1200, 600),
+        window_size_coef=1):
     '''
     Run the main Programm
     :param data_dirs: the directory containing the training data.
@@ -266,46 +287,60 @@ def run(data_dirs, model_save_file_path, image_dim, load_model_path=None, train=
     :param num_samples_to_load: number of samples to load from the data dir. None = all.
     :return: None
     '''
-    unnormalized_data = get_data(data_dirs, num_samples_to_load)
-    normalized_data = [normalize_data(image_data_un, position_data_un, rotation_data_un)
-                       for image_data_un, position_data_un, rotation_data_un in unnormalized_data ]
-    if black_n_white:
-        image_data = np.sum(image_data, -1) / 4
-
-    # TODO continue to implement new data structure
-    coordinate_inputs = network_inputs_from_coordinates(position_data, rotation_data)
-    flat_image_inputs = np.reshape(image_data, (-1, product(image_dim)))
-
     def get_closesd_image_to_coordinates(pos, rot):
         similarity_at_idx = []
         for im_pos, im_rot in zip(position_data, rotation_data):
             similarity_at_idx.append((np.sum(np.abs(pos - im_pos)) + np.sum(np.abs(rot - im_rot)) * 10))
-        return image_data[np.argmax(similarity_at_idx)]
+        return env[np.argmax(similarity_at_idx)]
 
     def black_n_white_to_rgb255(img):
         img = np.stack([img] * 3, -1)
         return img / np.max(img) * 255
 
-    def get_random_observation_input():
-        num_s = num_samples_to_load if num_samples_to_load else len(data_dirs)
-        return flat_image_inputs[random.randint(0, num_s - 1)]
+    def rgba_to_black_n_white(img):
+        return np.sum(img, -1) / 4
+
+    def get_random_observation_inputs(network_inputs, number_of_observations=1):
+        return random.choices(random.choice(network_inputs)[0], k=number_of_observations)
+
+    def unnormal_data_to_network_input(unnormalized_environment_data, black_n_white=False):
+        envs_data = []
+        for env in unnormalized_environment_data:
+            images = []
+            coordinates = []
+            for un_img, un_pos, un_rot in zip(env.images, env.coordinates, env.rotations):
+                img, pos, rot = normalize_data((un_img, un_pos, un_rot))
+                if black_n_white:
+                    img = rgba_to_black_n_white(img)
+                img = np.reshape(img, product(image_dim))
+                images.append(np.asarray(img))
+                coordinates.append(network_inputs_from_coordinates_single(pos, rot))
+            envs_data.append((images, coordinates))
+        return envs_data
+
+    window_size = collections.namedtuple('Rect', field_names='x y')(
+        x=int(window_size[0] * window_size_coef),
+        y=int(window_size[1] * window_size_coef))
+
+    unnormalized_environment_data = get_data_for_environments(data_dirs, num_envs_to_load, num_data_from_env)
+    network_inputs = unnormal_data_to_network_input(unnormalized_environment_data, black_n_white=True)
 
     model = None
     if load_model_path:
         model = keras.models.load_model(load_model_path)
-    if train or not load_model_path:
-        model = train_model(flat_image_inputs, coordinate_inputs, model_save_file_path, model, batch_size=batch_size)
+    else:
+        model = get_gqn_model(np.shape(network_inputs[0][0][0]), np.shape(network_inputs[0][1][0]))
+    if train:
+        model = train_model(network_inputs, model_save_file_path, model, batch_size=batch_size)
 
     if not run_environment:
         return
 
-    window_size = collections.namedtuple('Rect', field_names='x y')(x=1200*2, y=600*2)
-
     character_controller = CharacterController(position_data_un)
     img_drawer = ImgDrawer(window_size)
-    prev_time = 0
 
-    observation_input = get_random_observation_input()
+    # TODO make it work with network input strukture
+    observation_input = get_random_observation_inputs(network_inputs, 1)
     while True:
         output = model.predict([[observation_input], network_inputs_from_coordinates([character_controller.current_position], [character_controller.current_rotation_quaternion])])
         output = np.reshape(output[0], np.shape((image_data[0])))
@@ -327,7 +362,10 @@ def run(data_dirs, model_save_file_path, image_dim, load_model_path=None, train=
 
         keys = pygame.key.get_pressed()
         if keys[pygame.K_KP1]:
-            observation_input = get_random_observation_input()
+            observation_input = get_random_observation_inputs()
+
+        #TODO test if this is nessesary
+        pygame.event.pump()
 
 
 if __name__ == '__main__':
@@ -350,30 +388,40 @@ if __name__ == '__main__':
         2: 'GQN_SimpleRoom_withobj',
         3: 'GQN_SimpleRoom_RandomizedObjects_2',
     }
-    resolutions = {
+    image_resolutions = {
         32: '32x32',
         64: '64x64',
         128: '128x128',
         256: '256x256',
     }
-    resolutions = {
+    window_resolutions = {
         'uhd': (2400, 1200),
         'hd': (1200, 600),
     }
 
     def get_data_dir(key, resolution_key):
         for base_dir in data_base_dirs:
-            dir = (f'{base_dir}/{data_dirs.get(key)}_{resolutions.get(resolution_key)}')
+            dir = (f'{base_dir}\\{data_dirs.get(key)}\\{image_resolutions[resolution_key]}')
             if os.path.isdir(dir):
+                print(f'Found data in: {dir}')
                 return dir
+            else:
+                print(f'Data not found in: {dir}')
         raise OSError('None of specified data base dirs exist.')
 
     def get_img_dim_form_data_dir(dir):
-        dims = dir.split('_')[-1].split('x')
+        dims = dir.split('\\')[-1].split('x')
         return int(dims[0]), int(dims[1])
 
-    data_dir = get_data_dir(2, 32)
+    data_dir = get_data_dir(3, 32)
     img_dims = get_img_dim_form_data_dir(data_dir)
-    run(data_dirs=data_dir, load_model_path=model_names.get(0), image_dim=img_dims,
-        model_save_file_path=get_unique_model_save_name(img_dims, name='normal-run'), num_samples_to_load=None,
-        batch_size=None, run_environment=True, train=True)
+    run(data_dirs=data_dir,
+        load_model_path=model_names.get(0),
+        image_dim=img_dims,
+        model_save_file_path=get_unique_model_save_name(img_dims, name='normal-run'),
+        num_envs_to_load=2,
+        num_data_from_env=2,
+        batch_size=None,
+        run_environment=True,
+        train=True,
+        window_size=window_resolutions['hd'])
