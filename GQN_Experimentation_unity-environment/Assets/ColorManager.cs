051e62d6f776cc0ea2039d1c73046a241afcdcd1
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-12-08 16:24:29 +0100

    material setter

---
+++ GQN_Experimentation_unity-environment/Assets/ColorManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class UndoStep
+{
+    public Dictionary<GameObject, Material> colorDict = new Dictionary<GameObject, Material>();
+}
+
+public class ColorManager : MonoBehaviour {
+    public static ColorManager instance;
+    
+    public List<Material> materialPool = new List<Material>();
+    public List<Color> colorPool = new List<Color>()
+    {
+        new Color(1,0,0),
+        new Color(0,1,0),
+        new Color(1,0,1),
+        new Color(1,1,0),
+    };
+
+    public void SetSingelton()
+    {
+        instance = this;
+    }
+
+    void Start () {
+        SetSingelton();
+	}
+	
+    public void SetColorsOnMaterialPoolToPoolColors()
+    {
+        for (int i = 0; i < materialPool.Count; i++)
+        {
+            materialPool[i].color = colorPool[i];
+        }
+    }
+
+	public Material GetRandomMaterialFromPool()
+    {
+        return materialPool[Random.Range(0, materialPool.Count)];
+    }
+
+    public void SetRandomPoolMaterialOnGO(GameObject obj)
+    {
+        obj.GetComponent<Renderer>().sharedMaterial = GetRandomMaterialFromPool();
+    }
+}
---
+++ GQN_Experimentation_unity-environment/Assets/Editor/ColorManagerEditor.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ColorManager))]
+public class ColorManagerEditor : Editor
+{
+    Stack<Dictionary<GameObject, Material>> undos = new Stack<Dictionary<GameObject, Material>>();
+
+    public override void OnInspectorGUI()
+    {
+        var myTarget = (ColorManager)target;
+        myTarget.SetSingelton();
+        if (GUILayout.Button("Set Random Material On Selected Objects"))
+        {
+            var dict = new Dictionary<GameObject, Material>();
+            foreach (var go in Selection.gameObjects)
+            {
+                var rend = go.GetComponent<Renderer>();
+                if (rend != null)
+                {
+                    dict.Add(go, rend.sharedMaterial);
+                    ColorManager.instance.SetRandomPoolMaterialOnGO(go);
+                }
+            }
+            undos.Push(dict);
+        }
+        if (GUILayout.Button("Undo"))
+        {
+            if (undos.Count == 0)
+            {
+                Debug.Log("No More Undos");
+            }
+            else
+            {
+                var undo = undos.Pop();
+                foreach (var go in undo.Keys)
+                {
+                    go.GetComponent<Renderer>().sharedMaterial = undo[go];
+                }
+            }
+        }
+        if (GUILayout.Button("Set Materials to Color Pool Colors"))
+        {
+            myTarget.SetColorsOnMaterialPoolToPoolColors();
+        }
+
+        DrawDefaultInspector();
+    }
+}
