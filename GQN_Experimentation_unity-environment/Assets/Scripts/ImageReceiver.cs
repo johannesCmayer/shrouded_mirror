880dc1b7fd86e6d89576c9e72265f07e6d2df7a9
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-12-18 00:50:22 +0100

    job experiment

--- GQN_Experimentation_unity-environment/Assets/Scripts/ImageReceiver.cs
+++ GQN_Experimentation_unity-environment/Assets/Scripts/ImageReceiver.cs
@@ -7,6 +7,9 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Text;
 using UnityEngine.Rendering;
+using UnityEngine.Jobs;
+using Unity.Jobs;
+using Unity.Collections;
 
 public class ImageReceiver : MonoBehaviour {
 
@@ -24,7 +27,7 @@ public class ImageReceiver : MonoBehaviour {
 
     public RenderTexture renderTexture;
     
-
+    bool useJob = false;
 
     void Start ()
     {
@@ -51,6 +54,12 @@ public class ImageReceiver : MonoBehaviour {
                 nnScreenSizeX = streamTexture.width;
                 nnScreenSizeY = streamTexture.width;
             }            
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            useJob = !useJob;
+            print($"use job {useJob}");
         }
     }
 
@@ -80,14 +89,14 @@ public class ImageReceiver : MonoBehaviour {
         unityTexture.ReadPixels(new Rect(0, 0, Screen.width, Screen.height), 0, 0);
         Camera.main.targetTexture = null;
 
-        return ProcessUnityTexture(unityTexture, xSize, ySize);
+        
+
+        if (useJob)        
+            return StartCullRedAndRezizeJob(unityTexture, redCutoffValue, xSize, ySize);
+        else
+            return ProcessUnityTexture(unityTexture, xSize, ySize);
     }
 
-    Color32 NextCol(int i)
-    {
-        return new Color32((byte)(i % 255), 0, 0, 255);
-    }
-    int i = 0;
     Texture2D ProcessUnityTexture(Texture2D toProcess, int newXDim, int newYDim)
     {
         var currentPixels = toProcess.GetPixels32(0);
@@ -129,5 +138,101 @@ public class ImageReceiver : MonoBehaviour {
 
         Graphics.Blit(streamTexture, blitMaterial);
         Graphics.Blit(unityTex, blitMaterial);
+    }
+
+    struct CullRedAndRezizeJob : IJobParallelFor
+    {
+        public int redCutoffValue;
+        public int yDim;
+        public int xDim;
+        public NativeArray<byte> pixelsR,
+                                pixelsG,
+                                pixelsB,
+                                pixelsA;
+        public readonly int Length;
+
+        public void Execute(int i)
+        {
+            var combPixVal = pixelsR[i] + pixelsG[i] + pixelsB[i];
+            if (combPixVal < 1 ||
+                pixelsR[i] > redCutoffValue &&
+                pixelsG[i] < 10 &&
+                pixelsB[i] < 10)
+            {
+                pixelsR[i] = 0;
+                pixelsG[i] = 255;
+                pixelsB[i] = 0;
+                pixelsA[i] = 0;
+            }
+            else
+            {
+                pixelsA[i] = 255;
+            }
+        }
+    }
+
+    Texture2D StartCullRedAndRezizeJob(Texture2D inputTex, int redCutoffValue, int newXDim, int newYDim)
+    {
+        var pixels = inputTex.GetPixels32();
+        var inputTexSize = inputTex.width * inputTex.height;
+        var width = inputTex.width;
+        var height = inputTex.height;
+        var newTexSize = newXDim * newYDim;
+        NativeArray<byte> pixelsR = new NativeArray<byte>(newTexSize, Allocator.Temp);
+        NativeArray<byte> pixelsG = new NativeArray<byte>(newTexSize, Allocator.Temp);
+        NativeArray<byte> pixelsB = new NativeArray<byte>(newTexSize, Allocator.Temp);
+        NativeArray<byte> pixelsA = new NativeArray<byte>(newTexSize, Allocator.Temp);
+
+        for (int ypos = 0; ypos < newYDim; ypos += 1)
+        {
+            for (int xpos = 0; xpos < newXDim; xpos++)
+            {
+                var pixIdx = ypos * width * (height / newYDim) + xpos * (width / newXDim);
+                var newPixIdex = ypos * newXDim + xpos;
+
+                pixelsR[newPixIdex] = pixels[pixIdx].r;
+                pixelsG[newPixIdex] = pixels[pixIdx].g;
+                pixelsB[newPixIdex] = pixels[pixIdx].b;
+                pixelsA[newPixIdex] = pixels[pixIdx].a;
+            }
+        }
+
+        CullRedAndRezizeJob jobData = new CullRedAndRezizeJob
+        {
+            redCutoffValue = redCutoffValue,
+            yDim = inputTex.height,
+            xDim = inputTex.width,
+            pixelsR = pixelsR,
+            pixelsG = pixelsG,
+            pixelsB = pixelsB,
+            pixelsA = pixelsA,
+        };
+
+        JobHandle handle = jobData.Schedule(newTexSize, 1000);
+        handle.Complete();
+
+        var newTex = new Texture2D(newXDim, newYDim);
+        var newColors = new Color32[newTexSize];
+
+        for (int i = 0; i < newTexSize; i++)
+        {
+            newColors[i] = new Color32()
+            {
+                r = pixelsR[i],
+                g = pixelsG[i],
+                b = pixelsB[i],
+                a = pixelsA[i]
+            };
+        }
+
+        pixelsR.Dispose();
+        pixelsG.Dispose();
+        pixelsB.Dispose();
+        pixelsA.Dispose();
+
+        newTex.SetPixels32(newColors);
+        newTex.Apply();
+
+        return newTex;
     }
 }
