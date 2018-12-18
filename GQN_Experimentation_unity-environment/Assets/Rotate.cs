9405bf148a705f0bc7f7635b42bdcaa58d1d9e9b
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-12-18 19:40:37 +0100

    minor

--- GQN_Experimentation_unity-environment/Assets/Rotate.cs
+++ GQN_Experimentation_unity-environment/Assets/Rotate.cs
@@ -6,13 +6,6 @@ public class Rotate : MonoBehaviour
 {
     public Vector3 speed;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         transform.Rotate(speed.x, speed.y, speed.z);
