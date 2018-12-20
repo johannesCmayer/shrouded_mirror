741ba464e3ff027ffab78c98e8ef48248ee284e9
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-12-20 17:09:26 +0100

    add destroyable

---
+++ GQN_Experimentation_unity-environment/Assets/ShrinkAndDestroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShrinkAndDestroy : MonoBehaviour
+{
+    public float timeToStartShrink = 10;
+    public float shrinkDuration = 0.5f;
+
+    float timer;
+    Vector3 origScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        origScale = transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= timeToStartShrink)
+        {
+            transform.localScale = origScale * (1 - Mathf.Max(timer - timeToStartShrink, 0) / shrinkDuration);
+        }
+        if (timer >= shrinkDuration + timeToStartShrink)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
