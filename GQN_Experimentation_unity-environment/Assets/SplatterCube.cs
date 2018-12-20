acf8cc9dec877383070ae56bb1dfb183a5ab5061
Johannes C. Mayer <j.c.mayer240@gmail.com>
2018-12-20 18:52:53 +0100

    smoke sound

---
+++ GQN_Experimentation_unity-environment/Assets/SplatterCube.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplatterCube : MonoBehaviour
+{
+    public GameObject Fragment;
+    public int thirdRootOfNumberOfFragments = 3;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        
+    }
+
+    void SpawnSplatter()
+    {
+        for (int axis = 0; axis < 3; axis++)
+        {
+            for (int j = 0; j < thirdRootOfNumberOfFragments; j++)
+            {
+                var pos = transform.position[axis] + transform.localScale[axis] * Mathf.Lerp(-1, 1, j / thirdRootOfNumberOfFragments);
+            }
+        }
+    }
+
+    private void OnDestroy()
+    {
+        
+    }
+}
