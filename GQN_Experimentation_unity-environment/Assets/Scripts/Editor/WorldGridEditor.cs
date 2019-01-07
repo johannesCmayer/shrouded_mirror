db168e65d90febf168f86003b9ddeb0682f442ae
Johannes C. Mayer <j.c.mayer240@gmail.com>
2019-01-07 19:09:28 +0100

    adde maze

---
+++ GQN_Experimentation_unity-environment/Assets/Scripts/Editor/WorldGridEditor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(WorldGrid))]
+public class WorldGridEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        var myTarget = (WorldGrid)target;
+        if (GUILayout.Button("NewGrid"))
+            myTarget.CreateGrid();
+        DrawDefaultInspector();
+    }
+}
