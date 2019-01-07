f25adfe1c5da9de47073598936f36df656979ba4
Johannes C. Mayer <j.c.mayer240@gmail.com>
2019-01-07 14:52:10 +0100

    commit bevore assembly relocate

---
+++ GQN_Experimentation_unity-environment/Assets/Health.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{    
+    Vector3 spawnPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Die()
+    {
+        transform.position = spawnPosition;
+        var x = gameObject.GetComponent<RigidbodyFirstPersonController>();
+    }
+}
---
+++ GQN_Experimentation_unity-environment/Assets/KillPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void OnCollision(Collision c)
+    {
+        if (c.gameObject.CompareTag("Player"))
+        {
+            gameObject.GetComponent<Health>().Die();
+            
+        }
+    }
+}
