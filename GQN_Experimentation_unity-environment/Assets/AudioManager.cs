82da45dbe84985ab95776c73d3756f065464bd6a
Johannes C. Mayer <j.c.mayer240@gmail.com>
2019-01-08 09:10:54 +0100

    add audiomanager

---
+++ GQN_Experimentation_unity-environment/Assets/AudioManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public static AudioManager instance;
+    AudioSource myAudiosource;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        myAudiosource = GetComponent<AudioSource>();
+    }
+
+    public void PlayOneShot2D(AudioClip clip, float volume)
+    {
+        myAudiosource.PlayOneShot(clip, volume);
+    }
+}
